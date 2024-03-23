2b61759a5250ba096117f23a65034d83cddfb198
nucleoType98 <162495954+nucleoType98@users.noreply.github.com>
2024-03-23 19:45:18 +0100

    Update main.cpp
    added more comments

--- main.cpp
+++ main.cpp
@@ -7,10 +7,14 @@
 using namespace std;
 using namespace MainArgv;
 
+/** Function Pseudocode **/
+
 int main(int argc, char **argv) {
   
   vector<std::string> command_line_args;         // destination container for parsed command line arguments
   getArgv(argc, argv, &command_line_args);       // parsing and storing CLAs(command line arguments)
+
+  /** code... **/
   
-  return EXIT_SUCCESS; // or 0
+  return EXIT_SUCCESS;                           // or 0
 };
