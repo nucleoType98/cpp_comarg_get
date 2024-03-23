659e3290dc5834378eda24e38ceadd9311e0a4a9
nucleoType98 <162495954+nucleoType98@users.noreply.github.com>
2024-03-23 19:43:47 +0100

    updated the function name main.cpp
    the function name was wrong, adjusted now...

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@
 #include <stdio.h>
 
 using namespace std;
+using namespace MainArgv;
 
 int main(int argc, char **argv) {
   
   vector<std::string> command_line_args;         // destination container for parsed command line arguments
-  get_argv(argc, argv, command_line_args);       // parsing and storing CLAs(command line arguments)
+  getArgv(argc, argv, &command_line_args);       // parsing and storing CLAs(command line arguments)
   
   return EXIT_SUCCESS; // or 0
 };
