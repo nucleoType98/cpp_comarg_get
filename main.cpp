67cb47ad61392c8a4185de25c20d870bd1ae5252
nucleoType98 <162495954+nucleoType98@users.noreply.github.com>
2024-03-23 19:46:27 +0100

    Update main.cpp
    more more comments

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@ using namespace MainArgv;
 /** Function Pseudocode **/
 
 int main(int argc, char **argv) {
+
+
+  /** Note that argument list passed in the command line MUST follow this pattern: -command1 -command-2 -commandX -commandY 
   
   vector<std::string> command_line_args;         // destination container for parsed command line arguments
   getArgv(argc, argv, &command_line_args);       // parsing and storing CLAs(command line arguments)
