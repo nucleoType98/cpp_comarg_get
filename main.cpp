521251d0d9d90644deaec12d4e1f7793fb7904de
nucleoType98 <162495954+nucleoType98@users.noreply.github.com>
2024-03-23 19:41:57 +0100

    Update main.cpp
    better now...

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
 
+#include "get_argv.hpp"                         // include translation unit
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  
+  vector<std::string> command_line_args;         // destination container for parsed command line arguments
+  get_argv(argc, argv, command_line_args);       // parsing and storing CLAs(command line arguments)
+  
+  return EXIT_SUCCESS; // or 0
+};
